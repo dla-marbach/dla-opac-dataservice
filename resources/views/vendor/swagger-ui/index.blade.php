5c755c1d1c93d4baeffd66834a086eb8d2b662ff
Christopher Timm <timm@effective-webwork.de>
2025-03-23 19:45:23 +0100

    Use swagger css and js from laravel and not from an cdn

--- resources/views/vendor/swagger-ui/index.blade.php
+++ resources/views/vendor/swagger-ui/index.blade.php
@@ -1,51 +1,51 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>DLA Data+ API</title>
+    <title>DLA Data+ API</title>
 
-        <!--<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@latest/swagger-ui.css">-->
-        <link rel="stylesheet" href="/css/swagger-ui.css">
+    <!--<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@latest/swagger-ui.css">-->
+    <link href="{{ asset('css/swagger-ui.css') }}" rel="stylesheet">
 
-        <style>
-            html {
-                box-sizing: border-box;
-            }
+    <style>
+        html {
+            box-sizing: border-box;
+        }
 
-            *, *:before, *:after {
-                box-sizing: inherit;
-            }
+        *, *:before, *:after {
+            box-sizing: inherit;
+        }
 
-            body {
-                margin: 0;
-                background: #fafafa;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="swagger-ui"></div>
+        body {
+            margin: 0;
+            background: #fafafa;
+        }
+    </style>
+</head>
+<body>
+<div id="swagger-ui"></div>
 
-        <script src="/js/swagger-ui-bundle.js"></script>
+<script src="{{ asset('js/swagger-ui-bundle.js') }}"></script>
 
-        <script>
-            window.onload = function () {
-                window.ui = SwaggerUIBundle({
-                    url: '{{ route('swagger-openapi-json', [], false) }}',
-                    dom_id: '#swagger-ui',
-                    deepLinking: true,
-                    presets: [
-                        SwaggerUIBundle.presets.apis,
-                    ],
-                    layout: 'BaseLayout',
-                });
+<script>
+    window.onload = function () {
+        window.ui = SwaggerUIBundle({
+            url: '{{ route('swagger-openapi-json', [], false) }}',
+            dom_id: '#swagger-ui',
+            deepLinking: true,
+            presets: [
+                SwaggerUIBundle.presets.apis,
+            ],
+            layout: 'BaseLayout',
+        });
 
-                ui.initOAuth({
-                    clientId: '{{ config('swagger-ui.oauth.client_id') }}',
-                    clientSecret: '{{ config('swagger-ui.oauth.client_secret') }}',
-                });
-            };
-        </script>
-    </body>
+        ui.initOAuth({
+            clientId: '{{ config('swagger-ui.oauth.client_id') }}',
+            clientSecret: '{{ config('swagger-ui.oauth.client_secret') }}',
+        });
+    };
+</script>
+</body>
 </html>
